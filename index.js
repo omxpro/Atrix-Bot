e7c53eea653248f915f4a5c4a7c278d280c18f30
Daksh <99632106+DakshSoumya@users.noreply.github.com>
2022-03-24 09:57:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const aoijs = require("aoi.js")
+
+const bot = new aoijs.Bot({
+token: process.env.TOKEN,
+prefix: process.env.PREFIX,
+intents: ["GUILDS", "GUILD_MESSAGES"]
+})
+
+//Events
+bot.onMessage()
+
+//Command Example (ping)
+bot.command({
+name: "ping",
+code: `Pong! $pingms`
+})
+
+//Ready Event
+bot.readyCommand({
+    channel: "",
+    code: `$log[Ready on $userTag[$clientID]]`
+})
